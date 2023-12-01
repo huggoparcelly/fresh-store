537df25c6bb7253a83a893c01c01847cb5a097d7
huggoparcelly <h.parcelly@gmail.com>
2023-11-30 22:45:32 -0300

    add window to localStorage

--- utils/state.ts
+++ utils/state.ts
@@ -1,6 +1,9 @@
 import { Signal, signal } from "@preact/signals";
 import { Item } from "./items.ts";
 
+
+const { localStorage } = window
+
 export type AddToCartFunction = (item: Item) => void;
 export type RemoveFromCartFunction = (id: number) => void;
 
@@ -19,12 +22,7 @@ export type AppStateType = {
 
 function createAppState(): AppStateType {
     
-    let cartData = null;
-
-    if(typeof window !== 'undefined') {
-        cartData = localStorage.getItem("CART")
-    }
-    
+    const cartData = localStorage.getItem("CART")
     const cart = signal<CartItem[]>(cartData ? JSON.parse(cartData) : []);
 
     const addToCart: AddToCartFunction = (item: Item): void => {
@@ -36,10 +34,7 @@ function createAppState(): AppStateType {
         } else {
             cart.value = [...cart.value, {id: item.id, name: item.name, price:item.price, quantity: 1}]
         }
-        
-        if(typeof window !== 'undefined') {
-            localStorage.setItem("CART", JSON.stringify(cart.value));
-        }
+        localStorage.setItem("CART", JSON.stringify(cart.value));
     }
 
     const removeFromCart: RemoveFromCartFunction = (id: number): void => {
@@ -52,9 +47,7 @@ function createAppState(): AppStateType {
             } else {
                 cart.value = cart.value.filter((cartItem) => cartItem.id !== id);
             }
-            if(typeof window !== 'undefined') {
-                localStorage.setItem("CART", JSON.stringify(cart.value));
-            }
+            localStorage.setItem("CART", JSON.stringify(cart.value));
         }
     }
 
